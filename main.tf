f08f867d5bc588e328e8eab79b7f4b648381934f
James Borwick <james.borwick@icloud.com>
2023-08-30 20:41:16 +0100

    SSH key pair working

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "value"
+      source  = "hashicorp/aws"
+      version = "5.14.0"
     }
   }
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-051f7e7f6c2f40dc1"
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.my_key.key_name
+}
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "my-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINtb1Gbc6x9BWjeB2Vko8oo2VwqT5QOaEsXD2q/H0cAp jameswork@helenanamessair.lan"
 }
