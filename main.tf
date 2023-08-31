fd673ba9f9de8099aec07e6a71c80d44c78f76db
James Borwick <james.borwick@icloud.com>
2023-08-31 11:37:47 +0100

    Added count meta-argument

--- main.tf
+++ main.tf
@@ -7,13 +7,27 @@ terraform {
   }
 }
 
-resource "aws_instance" "my_instance" {
-  ami           = "ami-051f7e7f6c2f40dc1"
-  instance_type = "t3.micro"
-  key_name      = aws_key_pair.my_key.key_name
-}
-
 resource "aws_key_pair" "my_key" {
   key_name   = "my-key"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINtb1Gbc6x9BWjeB2Vko8oo2VwqT5QOaEsXD2q/H0cAp jameswork@helenanamessair.lan"
+}
+
+resource "aws_instance" "my_instance" {
+  count         = 2
+  ami           = "ami-051f7e7f6c2f40dc1"
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.my_key.key_name
+
+  user_data = <<-EOF
+    #!/bin/bash
+    hostnamectl set-hostname my-instance-${count.index}
+    echo 'export PS1="\h$ "' >> /home/ec2-user/.bashrc
+    sudo dnf install pip -y
+    sudo dnf install git -y
+    pip3 install ansible
+    EOF
+
+  tags = {
+    "Name" = "my-instance-${count.index}"
+  }
 }
